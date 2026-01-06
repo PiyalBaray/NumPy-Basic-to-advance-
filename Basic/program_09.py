db48d818ba51a2bbc0b83a2c47e1f954cbafbec2
PiyalBaray <piyalbaray314@gmail.com>
2026-01-06 20:11:44 +0530

    git commit -m "first commit"

---
+++ Basic/program_09.py
@@ -0,0 +1,12 @@
+# exponents and log use in numpy array
+import numpy as np
+
+a = np.arange(12).reshape(3,4)
+
+print("My numpy array:\n", a)
+print("\nLog value:\n", np.log(a))
+print("\nExponential value:\n", np.exp(a))
+
+
+
+
