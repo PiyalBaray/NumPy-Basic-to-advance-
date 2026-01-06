9b33234bf6c3cf0784500b5eb8ec37e1f67ede9d
PiyalBaray <piyalbaray314@gmail.com>
2026-01-06 20:11:44 +0530

    git commit -m "first commit"

---
+++ Advance/important function/program_06.py
@@ -0,0 +1,6 @@
+# unique()
+
+import numpy as np
+a=np.array([2,1,1,2,1,5,56,9,1,8,5,7,5,2,4,5,6])
+
+print(np.unique(a))
---
+++ Advance/important function/program_07.py
@@ -0,0 +1,6 @@
+# how to use where() function
+
+import numpy as np
+a=np.array([2,1,1,2,1,5,56,9,1,8,5,7,5,2,4,5,6])
+# which item is greater then 50  show this item index number
+print(np.where(a>50))
