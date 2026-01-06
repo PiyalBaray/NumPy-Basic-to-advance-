0cfa3e88419146d4c2f68797440174be2254ee8b
PiyalBaray <piyalbaray314@gmail.com>
2026-01-06 20:11:44 +0530

    git commit -m "first commit"

---
+++ Basic/program_05.py
@@ -0,0 +1,5 @@
+#
+import numpy as np
+
+a=np.arange(12).reshape(3,4)
+print("Max value of this numpy array:",np.max(a),"\nMin value of this numpy array:",np.min(a),"\naddition of all data in this numpy array:",np.sum(a),"\nMultiply  of all data in this numpy array:",np.prod(a))
---
+++ NumPy_Operations/Vector_operation/program_01.py
@@ -0,0 +1,7 @@
+# Vector (addition ,Subtraction,division,Multiply,power)
+import numpy as np
+
+a=np.arange(12).reshape(3,4)
+b=np.arange(12,24).reshape(3,4)
+print("1st numpy array is :\n",a,'\n',"2nd numpy array is :\n",b)
+print("addition  is :\n",a+b,"\nSubtraction is:\n",b-a,"\ndivision is:\n",a/b,"\nMultiply is:\n",a*b,"\npower is:\n")
