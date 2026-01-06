4359f373395706a73f5d35632f0777502dc961d5
PiyalBaray <piyalbaray314@gmail.com>
2026-01-06 20:11:44 +0530

    git commit -m "first commit"

---
+++ NumPy_Operations/Arithmetic_operation/program_01.py
@@ -0,0 +1,21 @@
+# NumPy multiply
+
+import numpy as np
+
+a=np.arange(12).reshape(3,4)
+print(a)
+print('Multiply result is :','\n',a*2)
+
+# Numpy add
+print('Addition result is :','\n',a+2)
+
+# Numpy sub
+print('Subtraction result is :','\n',a-2)
+
+#Numpy division
+print('division result is :','\n',a/2)
+
+# Numpy power
+print('power result is :','\n',a**2)
+
+
---
+++ NumPy_Operations/Relational_operation/program_01.py
@@ -0,0 +1,19 @@
+# Use all Relations operation
+
+import numpy as np
+
+a=np.arange(12).reshape(3,4)
+print(a)
+
+# use a<15
+print('a<15 :','\n',a<15)
+
+# use a>=15
+print('a>=15 :','\n',a>=15)
+
+#use a<=15
+print('a<=15 :','\n',a<=15)
+
+#use a==15
+print('a==15 :','\n',a==15)
+
